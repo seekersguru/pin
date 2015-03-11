a4d4b8d2a34e0f011babc7b9c51e836b5fbe6c61
ARNETWORKS <seekersguru@gmail.com>
2015-03-11 20:19:39 +0530

    angular socket deleted

--- angular-socketio-chat/angular-frontend/app/scripts/services/messageformatter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('chatApp')
-  .value('messageFormatter', function(date, nick, message) {
-    return date.toLocaleTimeString() + ' - ' + 
-           nick + ' - ' + 
-           message + '\n';
-    
-  });
