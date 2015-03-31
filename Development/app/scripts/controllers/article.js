f27a0bb8e54320b7b8135c256dc420b07118547a
Rituraj <riturajratan@gmail.com>
2015-04-01 00:14:41 +0530

    set some flow article

--- Development/app/scripts/controllers/article.js
+++ Development/app/scripts/controllers/article.js
@@ -218,12 +218,12 @@ angular.module('pinApp')
     if(form.$valid)
     {
       $scope.article.author=$rootScope.currentUser._id;
-      console.log($scope.article.tags);
-      console.log($scope.article);
-      var original=$scope.article.tags;
-      for (var t = 0; t < original.length; t++) {
-        $scope.article.tags[t] = original[t].text;
-      }
+      // console.log($scope.article.tags);
+      // console.log($scope.article);
+      // var original=$scope.article.tags;
+      // for (var t = 0; t < original.length; t++) {
+      //   $scope.article.tags[t] = original[t].text;
+      // }
       $scope.form.$setPristine();
       $http({ method: 'POST', url: '/api/articles',data:$scope.article }).
       success(function (data, status, headers, config) {
@@ -259,10 +259,10 @@ angular.module('pinApp')
       $scope.article.author=$rootScope.currentUser._id;
       console.log($scope.article.tags);
       console.log($scope.article);
-      var original=$scope.article.tags;
-      for (var t = 0; t < original.length; t++) {
-        $scope.article.tags[t] = original[t].text;
-      }
+      // var original=$scope.article.tags;
+      // for (var t = 0; t < original.length; t++) {
+      //   $scope.article.tags[t] = original[t].text;
+      // }
       $scope.articleDone=1;
       $scope.form.$setPristine();
       $http({ method: 'PUT', url: '/api/articles/'+$scope.article._id,data:$scope.article }).
@@ -312,7 +312,27 @@ angular.module('pinApp')
   // $scope.usingFlash = FileAPI && FileAPI.upload != null;
   $scope.fileReaderSupported = window.FileReader != null && (window.FileAPI == null || FileAPI.html5 != false);
 
+$scope.template=function(templateno){
+$scope.article.title="Set your Title Name";
+$scope.article.description="<p>Sed ut perspiciatis unde omnis iste natu error luptatem accusantium. dolorem laudantm, totam reaperiam, eaqu psa aeab illo inventore veriquasi architecto beatae vitae dicta sunt explicabo.</p>"+
 
+"<p>Lorem ipsum dolor it amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco oris nisi ut aliquipommodo consequat.</p>"+
+
+"<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"+
+
+"<b>berspiciatis unde omnis </b>"+
+
+"<p>Iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non Numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.</p>"+
+
+"<b> At vero eos et accusamus</b>"+
+
+"<p>Etusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod “Maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.” Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>"+
+
+"<b>Commodo Consequat</b>"+
+
+"<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugia</p>";
+$scope.article.tags=['tag1','tag2'];
+};
   $scope.uploadPic = function(files) {
     $scope.formUpload = true;
     if ($scope.mainFIle[0] !== null) {
@@ -332,10 +352,10 @@ angular.module('pinApp')
     $scope.article.author= $rootScope.currentUser._id;
     
     var original=$scope.article.tags;
-    $scope.article.tags=[];
-    for (var t = 0; t < original.length; t++) {
-      $scope.article.tags[t] = original[t].text;
-    }
+    // $scope.article.tags=[];
+    // for (var t = 0; t < original.length; t++) {
+    //   $scope.article.tags[t] = original[t].text;
+    // }
 
     file.upload = $upload.upload({
       url: '/api/articles',
@@ -454,11 +474,11 @@ angular.module('pinApp')
       // console.log(formData);
       
       $scope.article.author=$rootScope.currentUser._id;
-       var original=$scope.article.tags;
-        $scope.article.tags=[];
-        for (var t = 0; t < original.length; t++) {
-          $scope.article.tags[t] = original[t].text;
-         }
+       // var original=$scope.article.tags;
+       //  $scope.article.tags=[];
+       //  for (var t = 0; t < original.length; t++) {
+       //    $scope.article.tags[t] = original[t].text;
+       //   }
       $scope.form.$setPristine();
       $http({ method: 'POST', url: '/api/articles',data:$scope.article }).
       success(function (data, status, headers, config) {
