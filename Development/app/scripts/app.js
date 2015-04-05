2fe573017582c5677cc7c285b8ae33248714c50f
Rituraj <riturajratan@gmail.com>
2015-04-05 23:45:53 +0530

    set some js file

--- Development/app/scripts/app.js
+++ Development/app/scripts/app.js
@@ -55,6 +55,7 @@ angular.module('pinApp', [
   'duScroll',
   'angularFileUpload',
   'btford.socket-io',
+  'textAngular',
   'com.2fdevs.videogular',
   'com.2fdevs.videogular.plugins.controls',
   'com.2fdevs.videogular.plugins.overlayplay',
