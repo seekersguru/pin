0a4d9f504cbe2084c6b817c59f11c5c12d300e4c
Rituraj <riturajratan@gmail.com>
2015-08-02 17:13:50 +0530

    set discussion

--- Development/app/scripts/controllers/login.js
+++ Development/app/scripts/controllers/login.js
@@ -1,36 +1,35 @@
 'use strict';
 
 angular.module('pinApp')
-  .controller('LoginCtrl', function ($scope, Auth,$location, $rootScope) {
+  .controller('LoginCtrl', function($scope, Auth, $location, $rootScope) {
     $scope.user = {};
     $scope.errors = {};
     $rootScope.changeFooterNishant = 1;
     console.log(Object.keys($location.search())[0]);
     $scope.facebookLogin = Auth.facebookLogin;
     $scope.linkedinLogin = Auth.linkedinLogin;
-    
+
     $scope.registerStatus = Object.keys($location.search())[0] || 'test';
-    $scope.errormessage=$location.search()[$scope.registerStatus] || '';
-    $scope.field=Object.keys($location.search())[1] || 'field';
-    
+    $scope.errormessage = $location.search()[$scope.registerStatus] || '';
+    $scope.field = Object.keys($location.search())[1] || 'field';
+
 
     $scope.login = function(form) {
 
       $scope.submitted = true;
-      if(form.$valid) {
+      if (form.$valid) {
         Auth.login({
-          // I really hate doing this, I hope Angular JS guys and FF guys can solve this issue.
-          email: document.getElementById('username').value,   // Doing this becuase of Firefox issue of autofill
-          password: document.getElementById('password').value // not trigerring Angular JS update. FFBug ID: 950510
-        })
-        .then( function() {
-          // Logged in, redirect to home
-          if($rootScope.currentUser.role=='admin')
-            {
+            // I really hate doing this, I hope Angular JS guys and FF guys can solve this issue.
+            email: document.getElementById('username').value, // Doing this becuase of Firefox issue of autofill
+            password: document.getElementById('password').value // not trigerring Angular JS update. FFBug ID: 950510
+          })
+          .then(function() {
+            // Logged in, redirect to home
+            if ($rootScope.currentUser.role == 'admin') {
 
-            $location.path('/admin').search({'users':1});
+              $location.path('/discussion-listing');
 
-            }else{
+            } else {
               if ($rootScope.redirectPath) {
                 var path = $rootScope.redirectPath;
                 $rootScope.redirectPath = undefined;
@@ -39,20 +38,20 @@ angular.module('pinApp')
                 $location.path('/articles/01');
 
               }
-          }
-          $scope.registerStatus='test';
-        })
-        .catch( function(err) {
-          err = err.data;
-          $scope.errors = {};
-          // Update validity of form fields that match the mongoose errors
-          angular.forEach(err, function(error, field) {
-            form[error.field].$setValidity('mongoose', false);
-            $scope.errors[error.field] = error.message;
+            }
+            $scope.registerStatus = 'test';
+          })
+          .catch(function(err) {
+            err = err.data;
+            $scope.errors = {};
+            // Update validity of form fields that match the mongoose errors
+            angular.forEach(err, function(error, field) {
+              form[error.field].$setValidity('mongoose', false);
+              $scope.errors[error.field] = error.message;
+            });
+            $scope.registerStatus = 'test';
+
           });
-          $scope.registerStatus='test';
-          
-        });
       }
     };
   });
