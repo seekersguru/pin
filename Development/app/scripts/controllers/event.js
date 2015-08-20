7be25e8fec14950a00f55e0b9b6cf13f6b2b8816
Rituraj <riturajratan@gmail.com>
2015-08-20 13:23:28 +0530

    Set date

--- Development/app/scripts/controllers/event.js
+++ Development/app/scripts/controllers/event.js
@@ -4,11 +4,11 @@ angular.module('pinApp')
   .controller('EventListCtrl', function($scope, $rootScope, events) {
     angular.forEach(events, function(eventname, key) {
       var currentdate = new Date();
-
-      if (new Date(eventname.eventdate) < new Date(currentdate)) {
-        events.splice(key, 1);
-        console.log(key);
-      }
+      //
+      // if (new Date(eventname.eventdate) < new Date(currentdate)) {
+      //   events.splice(key, 1);
+      //   console.log(key);
+      // }
 
     });
     $scope.events = events;
