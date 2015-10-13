6c5eb3b7fb8c15a48e82c19f633178b8b4578581
Rituraj <riturajratan@gmail.com>
2015-10-13 22:36:33 +0530

    making mmi user filter

--- Development/app/scripts/controllers/adminpanel.js
+++ Development/app/scripts/controllers/adminpanel.js
@@ -49,6 +49,25 @@ angular.module('pinApp')
   .controller('AdminPanelCtrl', function($scope, User, MMIUser,Article, $http,
     $location, $window, $modal, Auth, $timeout) {
 
+
+
+      // function cb(start, end) {
+      //     $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+      // }
+      // cb(moment().subtract(29, 'days'), moment());
+      //
+      // $('#reportrange').daterangepicker({
+      //     ranges: {
+      //        'Today': [moment(), moment()],
+      //        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+      //        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+      //        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+      //        'This Month': [moment().startOf('month'), moment().endOf('month')],
+      //        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+      //     }
+      // }, cb);
+
+
     $scope.viewArticle = function(articleId) {
       window.open('/articles/view/' + articleId, '_blank');
     };
