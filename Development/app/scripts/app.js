2ff67c607d803c80249ca7f7f97d8c98162d7774
Rituraj <riturajratan@gmail.com>
2015-12-26 11:33:57 +0530

    set social icons

--- Development/app/scripts/app.js
+++ Development/app/scripts/app.js
@@ -44,7 +44,8 @@ angular.module('pinApp', [
   'com.2fdevs.videogular.plugins.controls',
   'com.2fdevs.videogular.plugins.overlayplay',
   'com.2fdevs.videogular.plugins.poster',
-  'ngGrid'
+  'ngGrid',
+  '720kb.socialshare'
   ])
 .value('nickName', 'anonymous')
 .config(function( $compileProvider ) {
