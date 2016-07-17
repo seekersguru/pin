39c050b9770d7aefb22f8ba1dec30afc49ad309b
Rituraj <rratan@sapient.com>
2016-07-17 17:20:51 +0530

    set player icon
    Signed-off-by: Rituraj <rratan@sapient.com>

--- Development/app/scripts/controllers/article.js
+++ Development/app/scripts/controllers/article.js
@@ -119,30 +119,35 @@ angular.module('pinApp')
         'caption': 'caption',
         'colorclass': 'blueprint-box',
         'videoclass': 'video-caption-yallow',
+        'videoheightclass': 'video-height-yallow',
         'bodyClass': 'blueprint'
       },
       'Essentials Foundation': {
         'caption': 'caption-red',
         'colorclass': 'red-box',
         'videoclass': 'video-caption-red',
+        'videoheightclass': 'video-height-red',
         'bodyClass': 'foundation'
       },
       'Growth Pillars': {
         'caption': 'caption-aqua',
         'colorclass': 'blue-box',
         'videoclass': 'video-caption-blue',
+        'videoheightclass': 'video-height-blue',
         'bodyClass': 'growth'
       },
       'Freedom Slab': {
         'caption': 'caption-pink',
         'colorclass': 'green-box',
         'videoclass': 'video-caption-green',
+        'videoheightclass': 'video-height-green',
         'bodyClass': 'freedom'
       },
       'Fun Money Roof': {
         'caption': 'caption-pink',
         'colorclass': 'yallow-box',
         'videoclass': 'video-caption-yallow',
+        'videoheightclass': 'video-height-yallow',
         'bodyClass': 'funroof'
       }
     };
